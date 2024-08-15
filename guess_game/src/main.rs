e4e99dab704d7c9a65963c44a3b3d8a953985cd2
per1cycle <pericycle.cc@gmail.com>
2024-08-15 19:09:54 +0800

    Update guess_game

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -2,30 +2,34 @@ use std::{cmp::Ordering, io};
 
 use rand::Rng;
 
-
 fn main() {
     println!("[INFO]: Start guessing game.");
-    // input number.
-    // notice the `mut` keyword
-    println!("[INFO]: Input a number:");
-    let mut _guess = String::new();
-    
+
     // choose a number.
     let _answer = rand::thread_rng().gen_range(1..=100);
-    println!("[INFO]: You pick: {}", _answer);
-    
-    io::stdin()
-    .read_line(&mut _guess)
-    .expect("Failed to readline.\n");
+    println!("[INFO]: Secrect number: {}", _answer);
 
-    // notice: dont forget .trim().
-    let _i_guess: u32 = _guess.trim().parse().expect("Please input a number.");
+    loop {
+        // input number.
+        // notice the `mut` keyword
+        println!("[INFO]: Input a number:");
+        let mut _guess = String::new();
 
-    println!("[INFO]: You guess: {}", _guess);
+        io::stdin()
+            .read_line(&mut _guess)
+            .expect("Failed to readline.\n");
 
-    match _i_guess.cmp(&_answer) {
-        Ordering::Equal => println!("[INFO]: Correct!"),
-        Ordering::Less => println!("[INFO]: a little small!"),
-        Ordering::Greater => println!("[INFO]: a little big!"),
+        // notice: dont forget .trim().
+        let _i_guess: u32 = _guess.trim().parse().expect("Please input a number.");
+        println!("[INFO]: You guess: {}", _i_guess);
+
+        match _i_guess.cmp(&_answer) {
+            Ordering::Greater => println!("[INFO]: a little big!"),
+            Ordering::Less => println!("[INFO]: a little small!"),
+            Ordering::Equal => {
+                println!("[INFO]: Correct!");
+                break;
+            }
+        }
     }
 }
